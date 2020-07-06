56310dd82236a4270f7bb67f53482db5246bf881
amp7368 <41219847+amp7368@users.noreply.github.com>
2020-07-06 14:23:54 -0400

    first base commit

---
+++ stock/src/main/java/aaron/stock/StockConstants.java
@@ -0,0 +1,6 @@
+package aaron.stock;
+
+public class StockConstants {
+    public final static String urlBase = "https://cloud.iexapis.com/stable";
+    public final static String urlIntraDay = urlBase + "/stock/{symbol}/intraday-prices";
+}
---
+++ stock/src/main/java/aaron/stock/StockMain.java
@@ -0,0 +1,11 @@
+package aaron.stock;
+
+import java.io.File;
+import java.io.IOException;
+
+public class StockMain {
+    public static void main(String[] args) throws IOException {
+
+
+    }
+}
